f91dc2af7293b8c1d9425ee202c68d5dd0aa212d
David Degras <david.degras@umb.edu>
2022-05-15 03:06:53 -0400

    Update extract.v.R

--- tensorMCCA/R/extract.v.R
+++ tensorMCCA/R/extract.v.R
@@ -25,26 +25,26 @@ extract.v.internal <- function(v, orders, modes)
 
 
 
-extract.v <- function(v, idx, sort = c("order", "block"))
-{
-	test <- check.arguments(v = v)
-	r <- NCOL(v[[1]][[1]])
-	idx <- as.integer(idx)
-	if (!all(idx %in% (1:r)))
-		stop(paste("Values in 'idx' must be integers between 1",
-			"and the common number of columns of each matrix in 'v'"))
-	sort <- match(sort)
-	m <- length(v)
-	d <- sapply(v, length)
-	if (sort == "block") {
-		for (i in 1:m) 
-			for (k in 1:d[i])
-				v[[i]][[k]] <- v[[i]][[k]][, idx, drop = FALSE]
-	} else {
-		vout <- replicate(r, vector("list", r)
-	}
+# extract.v <- function(v, idx, sort = c("order", "block"))
+# {
+	# test <- check.arguments(v = v)
+	# r <- NCOL(v[[1]][[1]])
+	# idx <- as.integer(idx)
+	# if (!all(idx %in% (1:r)))
+		# stop(paste("Values in 'idx' must be integers between 1",
+			# "and the common number of columns of each matrix in 'v'"))
+	# sort <- match(sort)
+	# m <- length(v)
+	# d <- sapply(v, length)
+	# if (sort == "block") {
+		# for (i in 1:m) 
+			# for (k in 1:d[i])
+				# v[[i]][[k]] <- v[[i]][[k]][, idx, drop = FALSE]
+	# } else {
+		# vout <- replicate(r, vector("list", r))
+	# }
 		
-	}
-	vout <- vector("list", ifelse(, m, length(idx)))
+	# }
+	# vout <- vector("list", ifelse(, m, length(idx)))
 	
-}
+# }
