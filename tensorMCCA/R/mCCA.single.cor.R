d46517a24f76187da1cd02edd3178ef186ef6dd0
David Degras <david.degras@umb.edu>
2022-05-15 03:07:13 -0400

    Update mCCA.single.cor.R

--- tensorMCCA/R/mCCA.single.cor.R
+++ tensorMCCA/R/mCCA.single.cor.R
@@ -78,14 +78,15 @@ for (it in 1:maxit) {
 
 	## Balance the canonical vectors of each dataset
 	## so they have equal norm
-	for (i in 1:m) {
-		nrmv <- sapply(v[[i]], function(x) sqrt(sum(x^2)))
-		nrmt <- prod(nrmv)
-		s <- if (nrmt < 1e-15) { 
-			numeric(d[i]) } else nrmt^(1/d[i]) / nrmv
-		for (k in 1:d[i])
-			v[[i]][[k]] <- s[k] * v[[i]][[k]]			
-	}
+	# for (i in 1:m) {
+		# nrmv <- sapply(v[[i]], function(x) sqrt(sum(x^2)))
+		# nrmt <- prod(nrmv)
+		# s <- if (nrmt < 1e-15) { 
+			# numeric(d[i]) } else nrmt^(1/d[i]) / nrmv
+		# for (k in 1:d[i])
+			# v[[i]][[k]] <- s[k] * v[[i]][[k]]			
+	# }
+	v <- scale.v(v, cnstr = "block")
 	
 	## Check convergence 
 	if (it > 1 && abs(objective[it+1]-objective[it]) <= 
