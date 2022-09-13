2eb2dad73a3f9008999bc391318c27d8e7697954
David Degras <david.degras@umb.edu>
2022-09-12 21:10:14 -0400

    Update deflation.R
    For deflation based on tensor weights, replaced reduction, which changes the data dimensions, by standard deflation.

--- tensorMCCA/R/deflation.R
+++ tensorMCCA/R/deflation.R
@@ -5,7 +5,7 @@
 ###########################################
 
 
-deflate.x <- function(x, v = NULL, score = NULL, ortho.mode = NULL, 
+deflate.x <- function(x, v = NULL, score = NULL, # ortho.mode = NULL, 
 	scope = c("block", "global"), check.args = TRUE)
 {
 scope <- match.arg(scope)
@@ -21,25 +21,25 @@ if (check.args) {
 	if (is.null(v)  == is.null(score)) 
 		stop(paste("Please specify exactly one of the arguments",
 		"'v' and 'score' and leave the other set to NULL."))
-	if (!is.null(v)) {		
-		if (is.null(ortho.mode))
-			stop("If 'v' is specified, then so must 'ortho.mode'.")
-		r <- NCOL(v)
-		ortho.mode <- drop(ortho.mode)
-		if (!((r == 1L && length(ortho.mode) == m) || 
-			(r > 1L) && identical(dim(ortho.mode), c(m, r))))
-			stop(paste("'ortho.mode' must be a matrix of lists",
-			"with dimensions matching those of 'v'"))
-		for (i in 1:m) {
-			modes <- unlist(
-				if (r == 1L) { ortho.mode[[i]] 
-				} else ortho.mode[i,])
-			if (!all(modes %in% (1:d[i])))
-				stop(paste("Values in 'ortho.mode[[i]]' or",
-				"'ortho.mode[i,]' not compatible with the",
-				"dimensions of 'x'."))
-		}	
-	}
+	# if (!is.null(v)) {		
+		# if (is.null(ortho.mode))
+			# stop("If 'v' is specified, then so must 'ortho.mode'.")
+		# r <- NCOL(v)
+		# ortho.mode <- drop(ortho.mode)
+		# if (!((r == 1L && length(ortho.mode) == m) || 
+			# (r > 1L) && identical(dim(ortho.mode), c(m, r))))
+			# stop(paste("'ortho.mode' must be a matrix of lists",
+			# "with dimensions matching those of 'v'"))
+		# for (i in 1:m) {
+			# modes <- unlist(
+				# if (r == 1L) { ortho.mode[[i]] 
+				# } else ortho.mode[i,])
+			# if (!all(modes %in% (1:d[i])))
+				# stop(paste("Values in 'ortho.mode[[i]]' or",
+				# "'ortho.mode[i,]' not compatible with the",
+				# "dimensions of 'x'."))
+		# }	
+	# }
 	if (!is.null(score)) {
 		stopifnot(is.vector(score) || is.matrix(score))
 		test1 <- (is.vector(score) && length(score) == m)
@@ -63,10 +63,22 @@ eps <- 1e-14
 
 ## Deflate with respect to canonical weight tensors
 if (!is.null(v)) {
-	cnstr <- set.ortho.mat(v = v, modes = ortho.mode)
-	x <- mapply(tnsr.mat.prod, x = x, mat = cnstr$mat,
-		modes = cnstr$modes, SIMPLIFY = FALSE)
+	if (!is.matrix(v)) v <- as.matrix(v)
+	r <- ncol(v)
+	for (i in 1:m) {
+		vi <- sapply(v[i,], function(vi) Reduce(kronecker, rev(vi)))
+		qrvi <- qr(vi)
+		if (qrvi$rank == 0) next
+		vi <- qr.Q(qrvi)[,1:qrvi$rank]
+		dim(x[[i]]) <- c(prod(p[[i]]), n)
+		x[[i]] <- x[[i]] - vi %*% crossprod(vi, x[[i]])
+		dim(x[[i]]) <- dimx[[i]]
+	}
 	return(x)
+	# cnstr <- set.ortho.mat(v = v, modes = ortho.mode)
+	# x <- mapply(tnsr.mat.prod, x = x, mat = cnstr$mat,
+		# modes = cnstr$modes, SIMPLIFY = FALSE)
+	# return(x)
 }
 
 
