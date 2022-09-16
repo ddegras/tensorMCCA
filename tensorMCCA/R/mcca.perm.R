cb82360887d080738f4647175afaef759593b807
David Degras <david.degras@umb.edu>
2022-09-15 20:17:29 -0400

    Update mcca.perm.R

--- tensorMCCA/R/mcca.perm.R
+++ tensorMCCA/R/mcca.perm.R
@@ -4,28 +4,13 @@ mcca.perm <- function(x, obj, l)
 for (name in names(obj$call.args))
 	assign(obj$call.args[[name]], name)
 v <- obj$v
-normvar <- switch(objective, cov = "norm", cor = "var")
-
-## Deflate data as needed
-if (l > 1 && ortho.type == "weight" && scale == "block") {
-	ortho.mode <- ortho.cnstr
-	ortho.cnstr <- set.ortho.mat(v = obj$v[,1:(l-1)], 
-		modes = ortho.mode[, 1:(l-1), l])
-	x <- deflate.x(x, v = v[, 1:(l-1)], 
-		ortho.mode = ortho.mode[, 1:(l-1), l], 
-		check.args = FALSE)	
-}
+normvar <- switch(objective.type, cov = "norm", cor = "var")
 
 ## Initialize canonical weights
 if (!is.null(init.val)) {
 	v0 <- if (is.matrix(init.val)) {
 		init.val[, min(l, ncol(init.val))]
 	} else init.val
-	if (l > 1 && ortho.type == "weight" && scale == "block") 
-		v0 <- tnsr.rk1.mat.prod(v0, mat = ortho.cnstr$mat, 
-			modes = ortho.cnstr$modes, transpose.mat = FALSE)
-	v0 <- scale.v(v0, type = normvar, scale = scale, 
-		check.args = FALSE)
 } else {
 	mcca.init <- switch(init.method, 
 		cca = mcca.init.cca, 
@@ -57,13 +42,13 @@ out <- if (objective == "cov" && optim == "bca" && scale == "block") {
 	mcca.cor.bca(x = x, v = v0, w = w, 
 		ortho = if (ortho == "score" && l > 1) {
 			ortho.cnstr[, 1:(l-1), drop = FALSE] } else NULL,
-		sweep = sweep, maxit = maxit, tol = tol, verbose = verbose)	
+		sweep = sweep, maxit = maxit, tol = tol, verbose = FALSE)	
 } else if (optim == "grad.scale") {
 	mcca.gradient.scale(x = x, v = v0, w = w, scale = scale, 
-		type = normvar, maxit = maxit, tol = tol, verbose = verbose)
+		type = normvar, maxit = maxit, tol = tol, verbose = FALSE)
 } else if (optim == "grad.rotate") {  
 	mcca.gradient.rotate(x = x, v = v0, w = w, 
-		maxit = maxit, tol = tol, verbose = verbose)
+		maxit = maxit, tol = tol, verbose = FALSE)
 }	
 
 out$objective
