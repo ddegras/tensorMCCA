8b6f2db530fa73dac24e9f9ba06d27db9e099802
David Degras <david.degras@umb.edu>
2022-09-23 23:07:39 -0400

    Update permutation.test.R

--- tensorMCCA/R/permutation.test.R
+++ tensorMCCA/R/permutation.test.R
@@ -7,11 +7,10 @@ n <- tail(dim(x[[1]]), 1)
 dimx <- lapply(x, dim)
 d <- sapply(dimx, length) - 1
 p <- mapply(head, dimx, d, SIMPLIFY = FALSE)
-r <- obj$call.args$r
 fields <- c("init.val", "objective.type", "ortho.type", 
-	"ortho.cnstr", "scale", "scope")
+	"ortho.cnstr", "r", "scale")
 for (name in fields) 
-	assign(obj$call.args[[name]], name)
+	assign(name, obj$call.args[[name]])
 if ((!is.null(init.val)) && (!is.matrix(init.val))) 
 	dim(init.val) <- c(m, 1)
 v <- obj$v
@@ -27,7 +26,7 @@ if (r > 1) {
 	xdfl <- vector("list", m * (r - 1))
 	dim(xdfl) <- c(m, r - 1)
 	for (l in 2:r) {
-		if (ortho.type == "weight" && scope == "block") {
+		if (ortho.type == "weight" && scale == "block") {
 			ortho.cnstr <- set.ortho.mat(v = v[,1:(l-1)], 
 				modes = ortho.mode[, 1:(l-1), l])
 			xdfl[,l-1] <- mapply(tnsr.mat.prod, x = x, 
@@ -37,14 +36,14 @@ if (r > 1) {
 				init.val[,l] <- tnsr.rk1.mat.prod(init.val[,l], 
 					mat = ortho.cnstr$mat, modes = ortho.cnstr$modes, 
 					transpose.mat = FALSE)
-		} else if (ortho.type == "weight" && scope == "global") {
+		} else if (ortho.type == "weight" && scale == "global") {
 			xdfl[,l-1] <- deflate.x(x, v = v[, 1:(l-1)], 
 				check.args = FALSE)
-		} else if (ortho.type == "score" && scope == "block") {
+		} else if (ortho.type == "score" && scale == "block") {
 			xdfl[,l-1] <- deflate.x(x, 
 				score = obj$block.score[,,1:(l-1)],
 				scope = "block", check.args = FALSE)	
-		} else if (ortho.type == "score" && scope == "global") {
+		} else if (ortho.type == "score" && scale == "global") {
 			xdfl[,l-1] <- deflate.x(x, 
 				score = obj$global.score[,1:(l-1)],
 				scope = "global", check.args = FALSE)			
@@ -74,12 +73,13 @@ if (parallel && require(foreach) && getDoParRegistered()) {
 			for (i in 1:m) {
 				dim(xx[[i]]) <- c(prod(p[[i]]), n)
 				xx[[i]] <- xx[[i]][, perm]
-				dim(x[[i]]) <- dimx[[i]]
+				dim(xx[[i]]) <- dimx[[i]]
 			}
 			rhopermj[l] <- mcca.perm(xx, obj, l)
 		}
 		rhopermj
 	}
+	rownames(rhoperm) <- NULL
 } else {
 	rhoperm <- matrix(, nperm, r)
 	for(j in 1:nperm) {
@@ -90,7 +90,7 @@ if (parallel && require(foreach) && getDoParRegistered()) {
 			for (i in 1:m) {
 				dim(xx[[i]]) <- c(prod(p[[i]]), n)
 				xx[[i]] <- xx[[i]][, perm]
-				dim(x[[i]]) <- dimx[[i]]
+				dim(xx[[i]]) <- dimx[[i]]
 			}	
 			rhoperm[j, l] <- mcca.perm(xx, obj, l)
 		}
@@ -99,10 +99,76 @@ if (parallel && require(foreach) && getDoParRegistered()) {
 
 ## p-values
 pval.uncorrected <- rowMeans(t(abs(rhoperm)) > abs(obj$objective))
-pval.corrected <- Reduce(max, uncorrected.pval, accumulate = TRUE)
+pval.corrected <- Reduce(max, pval.uncorrected, accumulate = TRUE)
 
 list(pval.corrected = pval.corrected, 
 	pval.uncorrected = pval.uncorrected,
 	objective = obj$objective, 
-	objective.perm = t(rhoperm))
+	objective.perm = rhoperm)
 }
+
+
+
+
+
+
+mcca.perm <- function(x, obj, l)
+{
+## Define variables with shorter names for easier handling
+for (name in names(obj$call.args))
+	assign(name, obj$call.args[[name]])
+v <- obj$v
+normvar <- switch(objective.type, cov = "norm", cor = "var")
+
+## Initialize canonical weights
+if (!is.null(init.val)) {
+	v0 <- if (is.matrix(init.val)) {
+		init.val[, min(l, ncol(init.val))]
+	} else init.val
+} else {
+	mcca.init <- switch(init.method, 
+		cca = mcca.init.cca, 
+		svd = mcca.init.svd, 
+		random = mcca.init.random)
+	init.args$x <- x
+	v0 <- do.call(mcca.init, init.args)
+}
+if (ortho.type == "score" && l > 1) {
+	v0 <- tnsr.rk1.ortho(v0, ortho.cnstr[, 1:(l-1), drop = FALSE], 
+		maxit = 100L, tol = 1e-6)
+	v0 <- scale.v(v0, type = normvar, x = x, check.args = FALSE)
+}
+
+## Run MCCA
+out <- if (objective.type == "cov" && optim == "bca" && 
+	scale == "block") {
+	mcca.cov.bca.block(x = x, v = v0, w = w, 
+		ortho = if (ortho.type == "score" && l > 1) {
+		ortho.cnstr[,1:(l-1)] } else NULL, 
+		sweep = sweep, maxit = maxit, tol = tol, 
+		verbose = FALSE)
+} else if (objective.type == "cov" && optim == "bca" && 
+	scale == "global") { 
+	mcca.cov.bca.global(x = x, v = v0, w = w, 
+		ortho = if (l == 1) { NULL 
+		} else if (ortho.type == "weight") { v[, 1:(l-1)] 
+		} else ortho.cnstr[,1:(l-1)], sweep = sweep, 
+		maxit = maxit, tol = tol, verbose = FALSE)
+} else if (objective.type == "cor" && optim == "bca") {
+	mcca.cor.bca(x = x, v = v0, w = w, 
+		ortho = if (ortho.type == "score" && l > 1) {
+			ortho.cnstr[, 1:(l-1), drop = FALSE] } else NULL,
+		sweep = sweep, maxit = maxit, tol = tol, verbose = FALSE)	
+} else if (optim == "grad.scale") {
+	mcca.gradient.scale(x = x, v = v0, w = w, scale = scale, 
+		type = normvar, maxit = maxit, tol = tol, verbose = FALSE)
+} else if (optim == "grad.rotate") {  
+	mcca.gradient.rotate(x = x, v = v0, w = w, 
+		maxit = maxit, tol = tol, verbose = FALSE)
+}	
+
+out$objective
+
+}
+
+
