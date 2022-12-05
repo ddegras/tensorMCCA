6d1fad19cadfaa38d4c17d022e68f4de3bb7a5fb
David Degras <david.degras@umb.edu>
2022-12-05 02:19:32 -0500

    Update simulations.functions.R

--- simulations.functions.R
+++ simulations.functions.R
@@ -2,40 +2,40 @@
 # Function to simulate canonical weights 
 #########################################
 
-simulate.v <- function(p, r, ortho = TRUE) {
-if (any(unlist(p) < 1)) 
-	stop("The values in 'dims' must be positive integers.")
-stopifnot(r >= 0)
-if (!is.list(p)) p <- list(p)
-p <- lapply(p, as.integer)
-r <- as.integer(r)
-if (r == 1) ortho <- FALSE
-if (ortho && any(unlist(p) < r))
-	warning("The generated canonical weight tensors",
-		" are not orthogonal to each other in all modes.")
-d <- sapply(p, length)
-m <- length(p)
-v <- vector("list", m * max(1, r))
-dim(v) <- c(m, max(1, r))
-for (i in 1:m) {
-	if (r == 0) {
-		v[[i]] <- lapply(p[[i]], numeric)
-		next
-	} 
-	for (k in 1:d[i]) {
-		vik <- matrix(runif(p[[i]][k] * r,-1, 1), p[[i]][k], r)
-		vik <- if (ortho) { 
-			svd(vik)$u
-		} else {
-			sweep(vik, 2, sqrt(colSums(vik^2)), "/")
-		}
-		nik <- ncol(vik)
-		for (l in 1:r) 
-			v[[i,l]][[k]] <- vik[, min(l, nik)]
-	}
-}
-v
-}
+# simulate.v <- function(p, r, ortho = TRUE) {
+# if (any(unlist(p) < 1)) 
+	# stop("The values in 'dims' must be positive integers.")
+# stopifnot(r >= 0)
+# if (!is.list(p)) p <- list(p)
+# p <- lapply(p, as.integer)
+# r <- as.integer(r)
+# if (r == 1) ortho <- FALSE
+# if (ortho && any(unlist(p) < r))
+	# warning("The generated canonical weight tensors",
+		# " are not orthogonal to each other in all modes.")
+# d <- sapply(p, length)
+# m <- length(p)
+# v <- vector("list", m * max(1, r))
+# dim(v) <- c(m, max(1, r))
+# for (i in 1:m) {
+	# if (r == 0) {
+		# v[[i]] <- lapply(p[[i]], numeric)
+		# next
+	# } 
+	# for (k in 1:d[i]) {
+		# vik <- matrix(runif(p[[i]][k] * r,-1, 1), p[[i]][k], r)
+		# vik <- if (ortho) { 
+			# svd(vik)$u
+		# } else {
+			# sweep(vik, 2, sqrt(colSums(vik^2)), "/")
+		# }
+		# nik <- ncol(vik)
+		# for (l in 1:r) 
+			# v[[i,l]][[k]] <- vik[, min(l, nik)]
+	# }
+# }
+# v
+# }
 
 
 ################################
@@ -44,70 +44,70 @@ v
 
 
 
-simulate.factor.model <- function(dimx, r, sigma2, xi, 
-	scale = c("block", "global"), ortho = FALSE)
-{
-## Data dimensions
-d <- sapply(dimx, length) - 1L
-m <- length(dimx)
-n <- sapply(dimx, tail, 1)
-if (any(n != n[1])) 
-	stop(paste("The last values of the components of",
-	"'dimx' must all be equal (= number of instances)."))
-n <- n[1]
-p <- mapply(head, dimx, d, SIMPLIFY = FALSE)
-pp <- sapply(p, prod)
-stopifnot(r >= 0)
-r0 <- r <- as.integer(r)
-sigma2 <- rep_len(sigma2, r)
-stopifnot(length(xi) == 1 || length(xi) == length(dimx))
-nxi <- length(xi)
-scale <- match.arg(scale)
+# # simulate.factor.model <- function(dimx, r, sigma2, xi, 
+	# scale = c("block", "global"), ortho = FALSE)
+# {
+# ## Data dimensions
+# d <- sapply(dimx, length) - 1L
+# m <- length(dimx)
+# n <- sapply(dimx, tail, 1)
+# if (any(n != n[1])) 
+	# stop(paste("The last values of the components of",
+	# "'dimx' must all be equal (= number of instances)."))
+# n <- n[1]
+# p <- mapply(head, dimx, d, SIMPLIFY = FALSE)
+# pp <- sapply(p, prod)
+# stopifnot(r >= 0)
+# r0 <- r <- as.integer(r)
+# sigma2 <- rep_len(sigma2, r)
+# stopifnot(length(xi) == 1 || length(xi) == length(dimx))
+# nxi <- length(xi)
+# scale <- match.arg(scale)
 
-## Outputs
-x <- vector("list", m)
-v <- if (scale == "block") {
-	simulate.v(p, r, ortho)
-} else if (ortho == FALSE) {
-	scale.v(simulate.v(p, r, FALSE), "norm", "global", 
-		check.args = FALSE)
-} else {
-	orthogonalize.global(simulate.v(p, r, FALSE))
-}
-if (r == 0) {
-	score <- numeric(n)
-} else {
-	score <- sapply(sqrt(sigma2), function(sig) rnorm(n, 0, sig))
-	score <- sweep(score, 2, colMeans(score))
-	score[is.nan(score)] <- 0 
-}
+# ## Outputs
+# x <- vector("list", m)
+# v <- if (scale == "block") {
+	# simulate.v(p, r, ortho)
+# } else if (ortho == FALSE) {
+	# scale.v(simulate.v(p, r, FALSE), "norm", "global", 
+		# check.args = FALSE)
+# } else {
+	# orthogonalize.global(simulate.v(p, r, FALSE))
+# }
+# if (r == 0) {
+	# score <- numeric(n)
+# } else {
+	# score <- sapply(sqrt(sigma2), function(sig) rnorm(n, 0, sig))
+	# # score <- sweep(score, 2, colMeans(score))
+	# # score[is.nan(score)] <- 0 
+# }
 
-for (i in 1:m) {	
-	## Simulate signal 
-	signal <- 0 
-	if (r > 0) {
-		vi <- matrix(, pp[i], r)
-		for (l in 1:r) 
-			vi[,l] <- Reduce(kronecker, rev(v[[i,l]]))
-		signal <- tcrossprod(vi, score)
-		dim(signal) <- c(p[[i]], n)
-	}	
-	## Simulate noise
-	if (is.numeric(xi)) {
-		noise <- rnorm(pp[i] * n, 
-			sd = sqrt(xi[min(i, nxi)]))	
-	} else {
-		if ((nxi == 1 && i == 1) || nxi > 1)
-			R <- chol(xi[[i]])
-		noise <- crossprod(R, matrix(rnorm(pp[i] * n), pp[i], n))
-	}
-	dim(noise) <- c(p[[i]], n)		
-	noise <- noise - as.vector(rowMeans(noise, dims = d[i]))
-	x[[i]] <- signal + noise
-}
+# for (i in 1:m) {	
+	# ## Simulate signal 
+	# signal <- 0 
+	# if (r > 0) {
+		# vi <- matrix(, pp[i], r)
+		# for (l in 1:r) 
+			# vi[,l] <- Reduce(kronecker, rev(v[[i,l]]))
+		# signal <- tcrossprod(vi, score)
+		# dim(signal) <- c(p[[i]], n)
+	# }	
+	# ## Simulate noise
+	# if (is.numeric(xi)) {
+		# noise <- rnorm(pp[i] * n, 
+			# sd = sqrt(xi[min(i, nxi)]))	
+	# } else {
+		# if ((nxi == 1 && i == 1) || nxi > 1)
+			# R <- chol(xi[[i]])
+		# noise <- crossprod(R, matrix(rnorm(pp[i] * n), pp[i], n))
+	# }
+	# dim(noise) <- c(p[[i]], n)		
+	# # noise <- noise - as.vector(rowMeans(noise, dims = d[i]))
+	# x[[i]] <- signal + noise
+# }
 
-list(x = x, v = v, score = score)
-}
+# list(x = x, v = v, score = score)
+# }
 
 
 ########################################
@@ -125,161 +125,115 @@ list(x = x, v = v, score = score)
 
 
 # Inputs
-# dimx:		dimensions of data tensors (list of length m)
-# r:		rank of data decomposition
-# sigma:	array of score covariances (m x m x r) 
+# dimx:			dimensions of data tensors (list of length m)
+# r:			rank of data decomposition
+# score.cov:	numeric vector of length r 
+#				or array of score covariances (m x m x r) 
+#				or list of r covariance matrices of size m x m
+# noise.cov:	numeric vector of length m
+#				or list of m covariance matrices of size prod(pp[i]) x prod(pp[i])
+#				where pp[i] is the length of the vectorized X_it 
 
 
-
-simulate.factor.model2 <- function(dimx, r, score.cov, noise.cov, 
-	scale = c("block", "global"), ortho = FALSE)
-{
-## Data dimensions
-d <- sapply(dimx, length) - 1L
-m <- length(dimx)
-n <- sapply(dimx, tail, 1)
-if (any(n != n[1])) 
-	stop(paste("The last values of the components of",
-	"'dimx' must all be equal (= number of instances)."))
-n <- n[1]
-p <- mapply(head, dimx, d, SIMPLIFY = FALSE)
-pp <- sapply(p, prod)
-stopifnot(r >= 0)
-r0 <- r <- as.integer(r)
-
-## Preprocess score covariance
-SIGMA <- score.cov
-if (is.numeric(SIGMA)) {
-	SIGMA <- rep_len(SIGMA, r)
-} else if (is.list(SIGMA)) {
-	SIGMA <- rep_len(SIGMA, r)
-	for (l in 1:r)
-		stopifnot(identical(dim(SIGMA[[l]]), c(m,m)))
-	SIGMA <- array(unlist(SIGMA), c(m, m, r))
-}
-stopifnot(is.numeric(SIGMA) || identical(dim(SIGMA), c(m,m,r)))
-
-## Preprocess noise covariance
-PSI <- noise.cov
-if (is.numeric(PSI)) {
-	PSI <- rep_len(PSI, r)
-} else if (is.list(PSI)) {
-	stopifnot(length(PSI) == r)
-	for (l in 1:r) 
-		stopifnot(identical(dim(PSI[[l]]), c(pp[l],pp[l])))
-}
-
-scale <- match.arg(scale)
-
-## Outputs
-x <- vector("list", m)
-v <- if (scale == "block") {
-	simulate.v(p, r, ortho)
-} else if (ortho == FALSE) {
-	scale.v(simulate.v(p, r, FALSE), "norm", "global", 
-		check.args = FALSE)
-} else {
-	orthogonalize.global(simulate.v(p, r, FALSE))
-}
-if (r == 0) {
-	score <- NULL
-} else if (is.numeric(SIGMA)) {
-	score <- sapply(sqrt(SIGMA), function(sig) rep(rnorm(n, 0, sig), m))
-	dim(score) <- c(n, m, r)
-} else {
-	score <- array(0, dim = c(n, m, r))
-	for (l in 1:r) {
-		eig <- eigen(SIGMA[[l]], TRUE)
-		stopifnot(all(eig$values >= -(1e-14)))
-		k <- sum(eig$values > 0)
-		if (k == 0) next
-		score[,,l] <- matrix(rnorm(n*k), n, k) %*% 
-			(t(eig$vectors[,1:k]) * eig$values[1:k]) 
-	}
-}
-
-for (i in 1:m) {	
-	## Simulate signal 
-	signal <- 0 
-	if (r > 0) {
-		for (l in 1:r) {
-			vil <- Reduce(kronecker, rev(v[[i,l]]))
-		signal <- signal + tcrossprod(vi, score[,i,l])
-		}
-		dim(signal) <- c(p[[i]], n)
-	}	
-	## Simulate noise
-	if (is.numeric(PSI)) {
-		noise <- rnorm(pp[i] * n, sd = sqrt(PSI[i]))	
-	} else {		
-		eig <- eigen(PSI[[i]], TRUE)
-		stopifnot(all(eig$values >= -(1e-14)))
-		k <- sum(eig$values > 0)
-		noise <- if (k == 0) {
-			numeric(pp[i] * n) } else {
-			matrix(rnorm(n*k), n, k) %*% 
-				(t(eig$vectors[,1:k]) * eig$values[1:k]) 
-		}
-	}
-	dim(noise) <- c(p[[i]], n)		
-	x[[i]] <- signal + noise
-}
-
-list(x = x, v = v, score = score)
-}
-
-
-
-
-# simulate.x2 <- function(dimx, r, sigma)
+# simulate.factor.model <- function(dimx, r, score.cov, noise.cov, 
+	# scale = c("block", "global"), ortho = FALSE)
 # {
 # ## Data dimensions
+# d <- sapply(dimx, length) - 1L
 # m <- length(dimx)
 # n <- sapply(dimx, tail, 1)
 # if (any(n != n[1])) 
-	# stop(paste("Last values in each component of list",
-	# "'dimx' must all be equal (number of replications)."))
+	# stop(paste("The last values of the components of",
+	# "'dimx' must all be equal (= number of instances)."))
 # n <- n[1]
-# p <- sapply(dimx, function(idx) idx[-length(idx)])
-# d <- sapply(dimx, length) - 1
+# p <- mapply(head, dimx, d, SIMPLIFY = FALSE)
+# pp <- sapply(p, prod)
+# stopifnot(r >= 0)
+# r0 <- r <- as.integer(r)
 
-# ## Check dimensions of sigma and replicate if needed
-# test <- is.matrix(sigma) && dim(sigma) == c(m,m)
-# stopifnot(test || all(dim(sigma) == c(m,m,r)))
-# if (test) sigma <- replicate(r, sigma)
+# ## Preprocess score covariance
+# SIGMA <- score.cov
+# if (is.vector(SIGMA)) {
+	# SIGMA <- rep_len(SIGMA, r)
+	# stopifnot(all(SIGMA >= 0))
+# } else if (is.list(SIGMA)) {
+	# SIGMA <- rep_len(SIGMA, r)
+	# for (l in 1:r)
+		# stopifnot(identical(dim(SIGMA[[l]]), c(m,m)))
+	# SIGMA <- array(unlist(SIGMA), c(m, m, r))
+# }
+# stopifnot(is.numeric(SIGMA) || identical(dim(SIGMA), c(m,m,r)))
+
+# ## Preprocess noise covariance
+# PSI <- noise.cov
+# if (is.numeric(PSI)) {
+	# PSI <- rep_len(PSI, r)
+# } else if (is.list(PSI)) {
+	# stopifnot(length(PSI) == r)
+	# for (l in 1:r) 
+		# stopifnot(identical(dim(PSI[[l]]), c(pp[l],pp[l])))
+# }
+
+# scale <- match.arg(scale)
 
 # ## Outputs
-# v <- vector("list", m * r)
-# dim(v) <- c(m, r)
-# x <- lapply(dimx, function(dims) array(0, dims))
-# block.scores <- array(dim = c(n,m,r))
-
-# ## Generate canonical vectors
-# rand.vec <- function(pp) {
-	# v <- runif(pp)
-	# v / sqrt(sum(v^2))
+# x <- vector("list", m)
+# v <- if (scale == "block") {
+	# simulate.v(p, r, ortho)
+# } else if (ortho == FALSE) {
+	# scale.v(simulate.v(p, r, FALSE), "norm", "global", 
+		# check.args = FALSE)
+# } else {
+	# orthogonalize.global(simulate.v(p, r, FALSE))
 # }
-# for (i in 1:m)
-# for (l in 1:r)
-# v[[i,l]] <- lapply(p[[i]], rand.vec)
-
-# ## Generate data
-# for (l in 1:r) {    
-	# ## Generate block scores
-	# R <- chol(sigma[,,l])
-	# tmp <- matrix(rnorm(m*n), m, n)
-	# block.scores[,,l] <- crossprod(tmp, R)
-            	
-	# ## Simulate data 
-	# for (i in 1:m) {
-		# vil <- Reduce(kronecker, rev(v[[i,l]]))
-		# vs <- tcrossprod(vil, block.scores[,i,l])
-		# x[[i]] <- x[[i]] + array(vs, dimx[[i]])
+# if (r == 0) {
+	# score <- NULL
+# } else if (is.vector(SIGMA)) {
+	# score <- sapply(sqrt(SIGMA), function(sig) rep(rnorm(n, 0, sig), m))
+	# dim(score) <- c(n, m, r)
+# } else {
+	# score <- array(0, dim = c(n, m, r))
+	# for (l in 1:r) {
+		# eig <- eigen(SIGMA[,,l], TRUE)
+		# stopifnot(all(eig$values >= -(1e-14)))
+		# k <- sum(eig$values > 0)
+		# if (k == 0) next
+		# score[,,l] <- matrix(rnorm(n*k), n, k) %*% 
+			# (t(eig$vectors[,1:k]) * sqrt(eig$values[1:k]))
 	# }
 # }
 
-# list(x = x, v = v, block.scores = block.scores)
+# for (i in 1:m) {	
+	# ## Simulate signal 
+	# signal <- 0 
+	# if (r > 0) {
+		# for (l in 1:r) {
+			# vil <- Reduce(kronecker, rev(v[[i,l]]))
+		# signal <- signal + tcrossprod(vil, score[,i,l])
+		# }
+		# dim(signal) <- c(p[[i]], n)
+	# }	
+	# ## Simulate noise
+	# if (is.numeric(PSI)) {
+		# noise <- rnorm(pp[i] * n, sd = sqrt(PSI[i]))	
+	# } else {		
+		# eig <- eigen(PSI[[i]], TRUE)
+		# stopifnot(all(eig$values >= -(1e-14)))
+		# k <- sum(eig$values > 0)
+		# noise <- if (k == 0) {
+			# numeric(pp[i] * n) } else {
+			# matrix(rnorm(n*k), n, k) %*% 
+				# (t(eig$vectors[,1:k]) * sqrt(eig$values[1:k]))
+		# }
+	# }
+	# dim(noise) <- c(p[[i]], n)		
+	# x[[i]] <- signal + noise
 # }
+
+# list(x = x, v = v, score = score)
+# }
+
+
 
 
 #####################################
