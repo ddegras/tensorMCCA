ef6e61bb2105d7ececd7e78a81349a5e11515e60
David Degras <david.degras@umb.edu>
2023-04-18 02:55:47 -0400

    Update mcca.cor.R

--- tensorMCCA/R/mcca.cor.R
+++ tensorMCCA/R/mcca.cor.R
@@ -97,7 +97,7 @@ call.args <- list(objective.type = "cor", r = NULL, w = w,
 	optim = optim, init.method = NULL, init.args = init.args, 
 	init.val = NULL, maxit = maxit, tol = tol, sweep = sweep, 
 	control = control) 
-if (ortho == "weight")
+if (ortho == "weight" && r > 1)
 	call.args$ortho.cnstr <- ortho.mode
 if (is.character(init)) { 
 	call.args$init.method <- init
