4ef6222a69d53985ea835428a98a957d7b2f89d8
David Degras <david.degras@umb.edu>
2023-10-19 01:15:02 +0200

    Update kkt.R

--- tensorMCCA/R/kkt.R
+++ tensorMCCA/R/kkt.R
@@ -7,52 +7,81 @@ kkt <- function(x, object)
 ## Check compatibility of arguments
 test <- check.arguments(x, object$v, object$call.args$w)
 
-## Extract fitted objects
-v <- object$v[,1]
-score <- object$block.score[,,1]
+## Extract relevant quantities
+v <- object$v
+score <- object$block.score
 w <- object$call.args$w
-objective <- object$objective[1]
+scale <- object$call.args$scale
+ortho <- object$call.args$ortho.type
+obj <- object$call.args$objective.type
 
 ## Calculate data dimensions
 m <- length(x)
 dimx <- lapply(x, dim)
-d <- sapply(dimx, length) - 1L
+d <- sapply(dimx, length) - 1L 
+p <- mapply(head, dimx, d, SIMPLIFY = FALSE)
 n <- tail(dimx[[1]], 1L)
+r <- NCOL(v)
 
-## Precalculate Lagrange multipliers
-objective.type <- object$call.args$objective.type
-scale <- object$call.args$scale
-if (objective.type == "cov" && scale == "global") {
-	lambda <- vector("list", m)
-	for (i in 1:m) {
-		if (d[i] == 1) {
-			lambda[[i]] <- rep(objective / m, d[i])
-		} else {
-			sqnrmv <- sapply(v[[i]], function(x) sum(x^2))
-			for (k in 1:d[i]) 
-				lambda[[i]][k] <- objective * prod(sqnrmv[-k]) / m 
-		}
-	}			
-} else {
-	lambda <- colSums(crossprod(score) * w)  / n
-	lambda <- mapply(rep, lambda, d, SIMPLIFY = FALSE)
+## Auxiliary function for gradient calculation
+if (ortho == "weight" && l > 1) {
+	gradfun <- function(x, y) {
+	d <- length(x)
+	if (d == 1) return(y)
+	cp <- numeric(d)
+	for (k in 1:d) cp[k] <- sum(x[[k]] * y[[k]])
+	out <- vector("list", d)
+	for (k in 1:d) out[[k]] <- prod(cp[-k]) * y[[k]]
+	unlist(out)	
+	}
 }
-	
-## Calculate KKT residuals 
-residuals <- vector("list", m)
+
+## Calculate gradients
+grad.obj <- grad.cnstr <- vector("list", m * r)
+dim(grad.obj) <- dim(grad.cnstr) <- c(m, r)
+sump <- sapply(p, sum)
 for (i in 1:m) {
-	for (k in 1:d[i]) {
-		## Calculate gradient of objective function
-		tvprod <- tnsr.vec.prod(x[[i]], v[[i]][-k], (1:d[i])[-k])
-		grad <- tvprod %*% score %*% (w[,i] / n) 		
-
-		residuals[[i]][[k]] <- if (objective.type == "cov") {
-			grad - lambda[[i]][k] * v[[i]][[k]]
+	for (l in 1:r) {
+		## Gradient of objective function
+		tvprod <- vector("list", d[i])
+		for (k in 1:d[i]) 
+			tvprod[[k]] <- tnsr.vec.prod(x = x[[i]], 
+				v = v[[i,l]][-k], modes = (1:d[i])[-k])
+		tvprod <- unlist(tvprod)
+		dim(tvprod) <- c(sump[i], n)
+		grad.obj[[i,l]] <- tvprod %*% score[,,l] %*% (w[,i] / n)
+		## Gradient of scaling constraint
+		mat <- matrix(, sump[i], l)
+		mat[,l] <- if (obj == "cor") {
+			tvprod %*% score[,i,l]	
 		} else {
-			grad - lambda[[i]][k] * (tvprod %*% score[,i] / n)	
+			unlist(v[[i,l]])
+		}
+		if (l == 1) {
+			grad.cnstr[[i,l]] <- mat
+			next
 		}		
-	}	
+		## Gradient of orthogonality constraints
+		if (ortho == "score") {
+			mat[,1:(l-1)] <- tvprod %*% score[,i,1:(l-1)]
+		} else {
+			for (ll in 1:(l-1))
+				mat[,ll] <- gradfun(v[[i,l]], v[[i,ll]])
+		}		
+		grad.cnstr[[i,l]] <- mat			
+	}
 }
 
-residuals
+## Calculate regression residuals
+residuals <- vector("list", m * r)
+dim(residuals) <- c(m, r)
+for (i in 1:m)
+for (l in 1:r)
+residuals[[i,l]] <- suppressWarnings(lsfit(grad.cnstr[[i,l]], 
+	grad.obj[[i,l]], NULL, FALSE)$residuals)
+	
+list(grad.objective = grad.obj, grad.constraints = grad.cnstr, 
+	residuals = residuals)	
 }
+
+
