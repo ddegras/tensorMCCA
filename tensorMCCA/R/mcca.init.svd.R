db78edeb511f084d487e3a3b4623e20ad3526a36
David Degras <david.degras@umb.edu>
2024-01-05 16:11:05 +0100

    Update mcca.init.svd.R

--- tensorMCCA/R/mcca.init.svd.R
+++ tensorMCCA/R/mcca.init.svd.R
@@ -7,7 +7,8 @@ eps <- 1e-14
 
 ## Data dimensions
 m <- length(x) # number of datasets 
-dimx <- lapply(x, dim) # full data dimensions
+dimfun <- function(x) if (is.vector(x)) c(1,length(x)) else dim(x)
+dimx <- lapply(x, dimfun) # full data dimensions
 p <- lapply(dimx, function(idx) idx[-length(idx)]) 
 # image dimensions (ignore last dimension of datasets = replications)
 d <- sapply(p, length)
@@ -20,70 +21,61 @@ if (is.null(w)) w <- matrix(1, m, m)
 ## Initialize canonical weights 
 v <- vector("list", m)
 
-## Unfold data along last mode (individuals/objects) and concatenate
+## Unfold data along last mode (individuals/objects) 
 xmat <- x
-for (i in 1:m) 
-	dim(xmat[[i]]) <- c(prod(p[[i]]), n)
-xmat <- do.call(rbind, xmat)
-
-## Data centering
-if (center) 
-	xmat <- xmat - rowMeans(xmat)
-
-## Catch trivial case
-if (all(xmat == 0)) {
-	for (i in 1:m) 
-		v[[i]] <- lapply(p[[i]], 
-			function(len) rep(1/sqrt(len), len))
-	return(v)
-}
-
-## Initial SVD
-dim.flag <- all(dim(xmat) > 2)
-error.flag <- FALSE
-if (dim.flag) {
-	svdx <- tryCatch(svds(xmat, k = 1, nv = 0)$u, 
-		error = function(e) NULL)
-	error.flag <- is.null(svdx)
-	if (!error.flag) xmat <- svdx 
-} 
-if ((!dim.flag) || error.flag)
-	xmat <- svd(xmat, nu = 1, nv = 0)$u
-dim(xmat) <- NULL
-
-
-## Iteratively unfold singular vectors and recalculate SVD
-lenx <- sapply(p, prod)
-end <- cumsum(lenx)
-start <- c(0, end[-m]) + 1
 for (i in 1:m) {
-	xi <- xmat[start[i]:end[i]]
-	if (d[i] > 1) dim(xi) <- p[[i]]
-	v[[i]] <- if (all(xi == 0)) {
-		lapply(p[[i]], function(len) rep(1/sqrt(len), len))
-	} else { lapply(hosvd(xi, 1)$factors, as.vector) }
+	dim(xmat[[i]]) <- c(prod(p[[i]]), n)
+	if (center) 
+		xmat[[i]] <- xmat[[i]] - rowMeans(xmat[[i]])	
 }
 
-
-## Scale initial canonical weights as needed	
-if (objective == "cov" && scale == "global") {
-	y <- canon.scores(x, v)
-	M <- crossprod(y) * (w/n)
-	a <- eigen(M, TRUE)$vectors[,1] * sqrt(m) 
-	s <- abs(a)^(1/d) # scaling factors
-	for (i in 1:m) {
-		if (a[i] < 0) v[[i]][[1]] <- -v[[i]][[1]]
-		v[[i]] <- lapply(v[[i]], "*", y = s[i]) }
-} else if (objective == "cor") {
-	y <- canon.scores(x, v)
-	y <- scale(y, scale = FALSE)
-	nrm <- sqrt(colMeans(y^2))
-	for (i in 1:m) {
-		v[[i]] <- if (nrm[i] > eps) {
-			lapply(v[[i]], "/", y = nrm[i]^(1/d[i]))
-		} else { lapply(p[[i]], numeric) }
+## Calculate pseudo-covariance covariance matrix
+pp <- sapply(p, prod)
+cpp <- cumsum(c(0, pp))
+covx <- matrix(0, cpp[m+1], cpp[m+1])
+for (i in 1:m) {
+	idxi <- (cpp[i]+1):cpp[i+1]
+	for (j in 1:i) {
+		if (w[i,j] == 0) next
+		idxj <- (cpp[j]+1):cpp[j+1]
+		covx[idxi,idxj] <- w[i,j] * tcrossprod(xmat[[i]], xmat[[j]])
+		if (j < i) covx[idxj,idxi] <- t(covx[idxi,idxj])
 	}
 }
+rm(xmat)
+
+## Initial EVD
+phi <- tryCatch(eigs_sym(covx, k = 1, which = "LA")$vectors, 
+		error = function(e) NULL)
+if (is.null(phi))
+	phi <- eigen(covx, TRUE)$vectors[,1]
+rm(covx)
+
+## Split eigenvector according to dataset dimensions, tensorize, 
+## and perform HOSVD
+covblock <- (objective == "cov" && scale == "block")
+for (i in 1:m) {
+	idxi <- (cpp[i]+1):cpp[i+1]
+	phii <- array(phi[idxi], p[[i]])
+	svdi <- hosvd(phii, 1)
+	v[[i]] <- lapply(svdi$factors, as.numeric) 
+	if (!covblock)
+		v[[i]][[1]] <- v[[i]][[1]] * as.numeric(svdi$core)
+}
+
+
+## Scale canonical weights and reorient as needed
+if (!covblock) {
+	v <- if (objective == "cov") {
+		scale.v(v, scale = scale, check.args = FALSE)
+	} else scale.v(v, "var", scale, x, FALSE)
+}
+score <- canon.scores(x, v, FALSE)
+flip <- reorient(score, w)$flip
+for (i in which(flip))
+	v[[i]][[1]] <- (-v[[i]][[1]])
+
+
 
 v
 }
