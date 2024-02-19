3f0331ddbf3a873e12467bf8671e6b27cedeb30e
David Degras <david.degras@umb.edu>
2024-02-19 17:05:53 +0100

    Update mcca.init.svd.R

--- tensorMCCA/R/mcca.init.svd.R
+++ tensorMCCA/R/mcca.init.svd.R
@@ -2,7 +2,7 @@ mcca.init.svd <- function(x, w = NULL, objective = c("cov", "cor"),
 	scale = c("block", "global"), center = TRUE)
 {
 ## Check argument x if required
-test <- check.arguments(x)
+test <- check.arguments(x, w)
 eps <- 1e-14
 
 ## Data dimensions
@@ -11,45 +11,96 @@ dimfun <- function(x) if (is.vector(x)) c(1,length(x)) else dim(x)
 dimx <- lapply(x, dimfun) # full data dimensions
 p <- lapply(dimx, function(idx) idx[-length(idx)]) 
 # image dimensions (ignore last dimension of datasets = replications)
+pp <- sapply(p, prod)
 d <- sapply(p, length)
 n <- tail(dimx[[1]], 1)
 
 objective <- match.arg(objective) # norm or variance constraints
 scale <- match.arg(scale)
-if (is.null(w)) w <- matrix(1, m, m)
+w <- if (is.null(w)) {
+	matrix(1/m^2, m, m) 
+} else {
+	(w + t(w)) / (2 * sum(w))
+}
 
 ## Initialize canonical weights 
 v <- vector("list", m)
 
 ## Unfold data along last mode (individuals/objects) 
+test <- (objective == "cor" || all(pp > n))
+if (test) {
+	svdx <- vector("list", m)
+	rankx <- numeric(m)
+}
 xmat <- x
 for (i in 1:m) {
-	dim(xmat[[i]]) <- c(prod(p[[i]]), n)
-	if (center) 
+	dim(xmat[[i]]) <- c(pp[i], n)
+	if (center)
 		xmat[[i]] <- xmat[[i]] - rowMeans(xmat[[i]])	
+	if (test) {
+		svdx[[i]] <- svd(xmat[[i]])
+		pos <- (svdx[[i]]$d > max(svdx[[i]]$d[1] * 1e-8, 1e-14))
+		rankx[i] <- sum(pos)
+		svdx[[i]]$u <- svdx[[i]]$u[,pos]
+		svdx[[i]]$d <- svdx[[i]]$d[pos]
+		svdx[[i]]$v <- svdx[[i]]$v[,pos]
+		xmat[[i]] <- switch(objective, 
+			cor = t(svdx[[i]]$v)	, 
+			cov = t(svdx[[i]]$v) * svdx[[i]]$d)
+	}	
 }
 
-## Calculate pseudo-covariance covariance matrix
-pp <- sapply(p, prod)
-cpp <- cumsum(c(0, pp))
-covx <- matrix(0, cpp[m+1], cpp[m+1])
-for (i in 1:m) {
-	idxi <- (cpp[i]+1):cpp[i+1]
-	for (j in 1:i) {
-		if (w[i,j] == 0) next
-		idxj <- (cpp[j]+1):cpp[j+1]
-		covx[idxi,idxj] <- w[i,j] * tcrossprod(xmat[[i]], xmat[[j]])
-		if (j < i) covx[idxj,idxi] <- t(covx[idxi,idxj])
+#  max a'x y'b    a'xx'a = 1.  
+# if p > n, x = QR  , set a = Q (R')^- aa.  aa' R^- Q' QR 
+# if n > p, x' = QR, a = R^- aa
+
+# x = UDV' a = U D^- aa
+# x' = QR. x = R^- z
+
+## Check separability of objective weights
+if (qr(w)$rank == 1) {
+	a <- colSums(w) # w = aa'
+	if (!all(a == a[1])) {
+		for (i in 1:m) 
+			xmat[[i]] <- xmat[[i]] * a[i]
 	}
+	xmat <- do.call(rbind, xmat)
+	phi <- tryCatch(svds(xmat, k = 1, nv = 0)$u, 
+		error = function(e) svd(xmat, 1, 0)$u)
+	rm(xmat)
+} else {
+	## Calculate pseudo-covariance matrix
+	nr <- sapply(xmat, nrow)
+	cumnr <- cumsum(c(0, nr))
+	covx <- matrix(0, cumnr[m+1], cumnr[m+1])
+	for (i in 1:m) {
+		idxi <- (cumnr[i]+1): cumnr[i+1]
+		for (j in 1:i) {
+			if (w[i,j] == 0) next
+			idxj <- (cumnr[j]+1):cumnr[j+1]
+			covx[idxi,idxj] <- w[i,j] * tcrossprod(xmat[[i]], xmat[[j]])
+			if (j < i) covx[idxj,idxi] <- t(covx[idxi,idxj])
+		}
+	}
+	rm(xmat)
+	## EVD
+	phi <- tryCatch(eigs_sym(covx, k = 1, which = "LA")$vectors, 
+			error = function(e) eigen(covx, TRUE)$vectors[,1])
+	rm(covx)
 }
-rm(xmat)
 
-## Initial EVD
-phi <- tryCatch(eigs_sym(covx, k = 1, which = "LA")$vectors, 
-		error = function(e) NULL)
-if (is.null(phi))
-	phi <- eigen(covx, TRUE)$vectors[,1]
-rm(covx)
+if (objective == "cor") {
+	cpp <- cumsum(c(0, pp))
+	phil <- vector("list", m)
+	for (i in 1:m) {
+		idxi <- (cpp[i]+1):cpp[i+1]
+		philid
+		phi[idxi] <- 
+	}		
+}	
+
+
+
 
 ## Split eigenvector according to dataset dimensions, tensorize, 
 ## and perform HOSVD
