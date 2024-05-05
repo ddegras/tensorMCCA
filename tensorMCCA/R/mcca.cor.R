7a0e014e0e39523c64c4c26b1da1e1cc57a554e8
David Degras <david.degras@umb.edu>
2024-05-05 11:59:55 +0200

    Update mcca.cor.R

--- tensorMCCA/R/mcca.cor.R
+++ tensorMCCA/R/mcca.cor.R
@@ -14,10 +14,9 @@ stopifnot(r > 0)
 
 ## Data dimensions
 m <- length(x) 
-dimx <- lapply(x, dim) 
+dimx <- lapply(x, dimfun) 
 d <- sapply(dimx, length) - 1L 
-p <- mapply(head, dimx, d, SIMPLIFY = FALSE) 
-pp <- sapply(p, prod)
+p <- mapply(head, dimx, d, SIMPLIFY = FALSE)
 n <- tail(dimx[[1]], 1) 
 
 ## Objective weights
@@ -42,14 +41,22 @@ if (is.character(init))
 	mcca.init <- switch(init, cca = mcca.init.cca, 
 		svd = mcca.init.svd, random = mcca.init.random)
 
-## Center data
+## Calculate tensor means across last dimension
+xbar <- vector("list", m)
+uncentered <- logical(m)
 for(i in 1:m) {
-    mu <- rowMeans(x[[i]], dims = d[i])
-    x[[i]] <- x[[i]] - as.vector(mu)
+    xbar[[i]] <- if (d[i] == 1) {
+    	mean(x[[i]])
+    } else {
+	    as.vector(rowMeans(x[[i]], dims = d[i]))
+	}
+	uncentered[i] <- any(abs(xbar) > 1e-16)
 }
-if (ortho == "weight") x0 <- x
+uncentered <- which(uncentered)
+# if (ortho == "weight") x0 <- x
 
-## Adjust number of canonical components as needed
+## Adjust number of canonical components
+pp <- sapply(p, prod)
 r <- switch(ortho, score = min(n - 1, max(pp), r),
 	weight = min(max(pp), r)) 
 
@@ -70,8 +77,8 @@ if (r > 1) {
 test <- (is.list(control) && !is.null(control$init))
 init.args <- NULL
 if (identical(init, "cca")) {
-	init.args <- list(k = NULL, w = w, 
-		objective = "cor", scope = "block", optim = NULL)
+	init.args <- list(k = NULL, w = w, objective = "cor", 
+		scope = "block", optim = NULL)
 	if (test) {
 		names. <- intersect(names(control), 
 			c("k", "optim"))
@@ -84,7 +91,6 @@ if (identical(init, "cca")) {
 }
 
 ## Create output objects 
-feasible <- logical(r)
 v <- vector("list", m * r) # canonical weights
 dim(v) <- c(m, r)
 block.score <- array(0, c(n, m, r)) 
@@ -104,100 +110,93 @@ if (is.character(init)) {
 	call.args$init.val <- as.matrix(init)
 }
 
-ones <- function(len) rep(1/sqrt(len), len) 
-
 ## MAIN LOOP
 
 for (l in 1:r) {	
 
 	if (verbose) cat("\n\nMCCA: Component", l, "\n")
+
+	## Create copy of original data before centering and deflating 
+	xl <- x
+	for (i in uncentered) 
+		xl[[i]] <- xl[[i]] - xbar[[i]]
 	
-	## Prepare orthogonality constraints
+	## Prepare orthogonality constraints and deflate data
 	if (l > 1) {
-		if (ortho == "weight") {
-			## Deflate + reduce data
-			ortho.cnstr <- set.ortho.mat(v = v[, 1:(l-1)], 
-				modes = ortho.mode[, 1:(l-1), l])
-			if (any(ortho.cnstr$vzero)) {
-				v[,l] <- relist(lapply(unlist(p), numeric), p)
-				next
-			}
-			x <- mapply(tnsr.mat.prod, x = x0, 
-				mat = ortho.cnstr$mat, modes = ortho.cnstr$modes, 
-				SIMPLIFY = FALSE)
-		} else if (ortho == "score") {
-			## Calculate orthogonality constraints explicitly	
+		if (ortho == "score") {
 			for (i in 1:m)
 				ortho.cnstr[[i,l-1]] <- tnsr.vec.prod(x[[i]], 
 					block.score[,i,l-1], d[i]+1)
-		}
+			xl <- deflate.x(xl, ortho.cnstr[,1:(l-1)])
+		} else if (ortho == "weight") {
+			ortho.cnstr <- set.ortho.mat(v = v[, 1:(l-1)], 
+				modes = ortho.mode[, 1:(l-1), l])
+			for (i in 1:m)
+				xl[[i]] <- tnsr.mat.prod(xl[[i]], ortho.cnstr$mat[[i]],
+					ortho.cnstr$modes[[i]])	
+		} 
 	}
 
-	## More data preprocessing
-	if (l == 1 || ortho == "weight") {
-		## Remove datasets that are zero 
-		xzero <- sapply(x, function(a) all(a == 0))
-		xnzero <- !xzero
-		if (any(xzero)) x <- x[xnzero]
-		## Drop singleton dimensions
-		if (any(xnzero)) {	
-			dimxx <- lapply(x, 
-				function(xx) if (is.array(xx)) dim(xx) else c(1,n))
-			dimv <- lapply(dimxx, function(x) x[-length(x)])
-			for (i in 1:length(x)) {
-				if (all(dimv[[i]] > 1)) next
-				x[[i]] <- drop(x[[i]])
-				if (!is.array(x[[i]]))
-					dim(x[[i]]) <- c(1, n)
-			}
-		}
+	## Remove datasets that are zero 
+	xzero <- sapply(xl, function(a) all(a == 0))
+	xnzero <- !xzero
+	if (any(xzero)) xl <- xl[xnzero]
+	wl <- w[xnzero, xnzero]
+
+	## Drop singleton dimensions
+	for (i in seq_along(xl)) {	
+		dimxli <- dim(xl[[i]])
+		if (is.null(dimxli) || all(dimxli > 1)) next
+		dim(xl[[i]]) <- drop(dimxli)
 	}
 
-	## Trivial case: all or all but one datasets equal to zero
-	if (sum(xzero) >= (m-1)) {
+	## Trivial case: all datasets equal to zero
+	if (all(xzero)) {
 		v[,l] <- relist(lapply(unlist(p), numeric), p)
-		if (l == 1) objective[1] <- 0
+		objective[l] <- 0
 		if (l > 1 && ortho == "weight") next else break
 	}
 	
-	## Specify canonical weights for datasets equal to zero
+	## Set canonical weights for datasets equal to zero
 	for (i in which(xzero)) 
 		v[[i,l]] <- lapply(p[[i]], numeric)
 		
+	## Specify orthogonality constraints in optimization
+	cnstr <- if (l == 1 || ortho == "weight") {
+		NULL
+	} else if (ortho == "score") { 
+		ortho.cnstr[xnzero, 1:(l-1), drop = FALSE]
+	}
+
 	## Initialize canonical weights
 	if (is.character(init)) {
-		init.args$x <- if (ortho == "score" && l > 1) {
-			deflate.x(x, score = block.score[,,1:(l-1)], 
-				scope = "block", check.args = FALSE)
-			} else x
-		if (init == "cca") init.args$w <- w[xnzero, xnzero]			
+		init.args$x <- xl
+		if (init %in% c("cca","svd")) 
+			init.args$w <- wl			
 		v0 <- do.call(mcca.init, init.args)
-	} else { # case: 'init' is a list
+	} else if (is.list(init)) { 
 		v0 <- if (is.vector(init)) {
-			init[xnzero] } else { init[xnzero, min(l, ncol(init))] }
-		if (ortho == "weight" && l > 1) 
+			init[xnzero] 
+		} else { 
+			init[xnzero, min(l, ncol(init))]
+		}
+		if (l > 1 && ortho == "weight") 
 			v0 <- tnsr.rk1.mat.prod(v0, mat = ortho.cnstr$mat[xnzero], 
 				modes = ortho.cnstr$modes[xnzero], transpose.mat = FALSE)
-		v0 <- scale.v(v0, type = "var", x = x, check.args = FALSE)
 	}
-	if (ortho == "score" && l > 1) {
-		v0 <- tnsr.rk1.ortho(v0, ortho.cnstr[xnzero, 1:(l-1), drop = FALSE], 
-			maxit = 100L, tol = 1e-6)
-		v0 <- scale.v(v0, type = "var", x = x, check.args = FALSE)
+		
+	## Enforce scaling and orthogonality constraints
+	if (l > 1) {
+		v0 <- tnsr.rk1.ortho(v0, cnstr, maxit = 100L, tol = 1e-6)
 	}
-	if (all(unlist(v0) == 0)) {
-		v[,l] <- relist(lapply(unlist(p), numeric), p)
-		next
-	}
+	v0 <- scale.v(v0, type = "var", x = x, check.args = FALSE)
 	
-	## Run MCCA
+	## Run TMCCA
 	out <- if (optim == "bca") {
-		mcca.cor.bca(x = x, v = v0, w = w[xnzero,xnzero], 
-		ortho = if (ortho == "score" && l > 1) {
-			ortho.cnstr[xnzero, 1:(l-1), drop = FALSE] } else NULL,
+		mcca.cor.bca(x = x, v = v0, w = wl, ortho = cnstr,
 		sweep = sweep, maxit = maxit, tol = tol, verbose = verbose)
 	} else { 
-		mcca.gradient.scale(x = x, v = v0, w = w[xnzero,xnzero], 
+		mcca.gradient.scale(x = x, v = v0, w = wl, 
 			scope = "block", type = "var", maxit = maxit, 
 			tol = tol, verbose = verbose)
 	}
@@ -206,16 +205,13 @@ for (l in 1:r) {
 	global.score[,l] <- rowMeans(block.score[,xnzero,l])	
 	iters[l] <- out$iters	
 	
-	## Reinsert singleton dimensions in canonical weights if needed
-	for (i in 1:length(x)) {
-		singleton <- (dimv[[i]] == 1)
-		if (!any(singleton)) next			
-		vil <- replicate(length(dimv[[i]]), 1, FALSE)
-		if (all(singleton)) {
-			vil[[1]] <- out$v[[i]][[1]]
-		} else {
-			vil[!singleton] <- out$v[[i]]			
-		}
+	## Post-process canonical weights
+	for (i in seq_along(xl)) {
+		dimxli <- dimxl[[i]]
+		if (is.null(dimxli) || all(dimxli > 1)) next
+		vil <- vector("list", length(dimxli))
+		vil[dimxli == 1] <- list(1)
+		vil[dimxli > 1] <- out$v[[i]]		
 		out$v[[i]] <- vil		
 	}
 	
