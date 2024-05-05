6daae02660b4b180038bedc2c4eba3377d66435b
David Degras <david.degras@umb.edu>
2024-05-05 11:59:48 +0200

    Update mcca.cov.R

--- tensorMCCA/R/mcca.cov.R
+++ tensorMCCA/R/mcca.cov.R
@@ -13,7 +13,6 @@ r <- as.integer(r)
 
 ## Data dimensions
 m <- length(x)
-dimfun <- function(x) if (is.vector(x)) c(1,length(x)) else dim(x)
 dimx <- lapply(x, dimfun) 
 d <- sapply(dimx, length) - 1L 
 p <- mapply(head, dimx, d, SIMPLIFY = FALSE) 
@@ -57,8 +56,6 @@ for(i in 1:m) {
 	uncentered[i] <- any(abs(xbar) > 1e-16)
 }
 uncentered <- which(uncentered)
-
-# if (ortho == "weight") x0 <- x
 
 ## Adjust number of canonical components 
 pp <- sapply(p, prod)
@@ -121,8 +118,6 @@ if (is.character(init)) {
 	call.args$init.val <- as.matrix(init)
 }
 
-ones <- function(len) rep(1/sqrt(len), len) 
-
 ## MAIN LOOP
 for (l in 1:r) {	
 	
@@ -133,29 +128,24 @@ for (l in 1:r) {
 	for (i in uncentered) 
 		xl[[i]] <- xl[[i]] - xbar[[i]]
 	
-	## Deflate data as needed
+	## Prepare orthogonality constraints and deflate data
 	if (l > 1) { 	
 		if (ortho == "score" && scope == "block") { 
-			score <- block.score[,, 1:(l-1), drop = FALSE]
-			## Calculate orthogonality constraints explicitly	
 			for (i in 1:m)
 				ortho.cnstr[[i,l-1]] <- tnsr.vec.prod(xl[[i]], 
-					score[,i,l-1], d[i]+1)
+					block.score[,i,l-1], d[i]+1)
 			xl <- deflate.x(xl, ortho.cnstr[,1:(l-1)])
 		} else if (ortho == "score" && scope == "global") { 
-			score <- global.score[,1:(l-1), drop = FALSE]
 			for (i in 1:m)
 				ortho.cnstr[[i,l-1]] <- tnsr.vec.prod(xl[[i]], 
-					score[,l-1], d[i]+1)
+					global.score[,l-1], d[i]+1)
 			xl <- deflate.x(xl, ortho.cnstr[,1:(l-1)], "global")
 		} else if (ortho == "weight" && scope == "block") {
-			ortho.cnstr <- set.ortho.mat(v = v[,1:(l-1)], 
-				modes = ortho.mode[, 1:(l-1), l])
-			vzero <- ortho.cnstr$vzero
-			xl[vzero] <- 0				
-			for (i in which(!vzero)) 
-				xl[[i]] <- tnsr.mat.prod(xl[[i]], 
-					ortho.cnstr$mat[[i]], ortho.cnstr$modes[[i]])	
+			ortho.cnstr <- set.ortho.mat(v = v[,1:(l-1), drop=FALSE], 
+				modes = ortho.mode[, 1:(l-1), l, drop=FALSE])
+			for (i in 1:m)
+				xl[[i]] <- tnsr.mat.prod(xl[[i]], ortho.cnstr$mat[[i]],
+					ortho.cnstr$modes[[i]])	
 		} 
 	}
 	
@@ -165,13 +155,6 @@ for (l in 1:r) {
 	if (any(xzero)) xl <- xl[xnzero]
 	wl <- w[xnzero, xnzero]
 	
-	## Drop singleton dimensions
-	for (i in seq_along(xl)) {	
-		dimxli <- dim(xl[[i]])
-		if (is.null(dimxli) || all(dimxli > 1)) next
-		dim(xl[[i]]) <- drop(dimxli)
-	}
-
 	## Trivial case: all datasets equal to zero
 	if (all(xzero)) {
 		v[,l] <- relist(lapply(unlist(p), numeric), p)
@@ -179,38 +162,30 @@ for (l in 1:r) {
 		if (l > 1 && ortho == "weight" && scope == "block") 
 			next else break
 	}
+
+	## Drop singleton dimensions
+	dimxl <- vector("list", sum(xnzero))
+	for (i in seq_along(xl)) {
+		dimxl[[i]] <- dim(xl[[i]])
+		if (is.null(dimxli) || all(dimxli > 1)) next
+		dim(xl[[i]]) <- drop(dimxli)
+	}
 	
-	## Specify canonical weights for datasets equal to zero
+	## Set canonical weights for datasets equal to zero
 	for (i in which(xzero)) 
 		v[[i,l]] <- lapply(p[[i]], numeric)
 
 	## Specify orthogonality constraints in optimization
 	cnstr <- if (l == 1) {
-	  NULL
+		NULL
 	} else if (scope == "block" && ortho == "weight") { 
-	  NULL 
+		NULL 
 	} else if (scope == "global" && ortho == "weight") {
-	  v[xnzero,1:(l-1)]
+		v[xnzero,1:(l-1)]
 	} else if (ortho == "score") {
-	  ortho.cnstr[xnzero,1:(l-1)] 
+		ortho.cnstr[xnzero,1:(l-1)] 
   }
 	
-	# ## Trivial case: all but one dataset are zero
-	# @@@@ FIGURE OUT WHAT TO DO HERE
-	# if (sum(xnzero) == 1) {
-		# i <- which(xnzero)
-		# v[[i,l]] <- tnsr.rk1.wrapper(x, dimx[[i]])	
-		# if (l > 1 && ortho == "weight" && scope == "block") {
-		# v[[i,l]] <- tnsr.rk1.mat.prod(v = v[[i,l]], 
-			# mat = ortho.cnstr$mat[[i]], modes = ortho.cnstr$modes[[i]], 
-			# transpose.mat = TRUE)
-		# }
-		# block.score[,,l] <- canon.scores(x0, v[,l], FALSE)
-		# global.score[,l] <- rowMeans(block.score[,,l])
-		# objective[l] <- sum(crossprod(block.score[,,l]) * w) / n
-		# next	
-	# }
-
 	## Initialize canonical weights
 	if (is.character(init)) {
 		init.args$x <- xl
@@ -227,6 +202,7 @@ for (l in 1:r) {
 			v0 <- tnsr.rk1.mat.prod(v0, mat = ortho.cnstr$mat[xnzero], 
 				modes = ortho.cnstr$modes[xnzero], transpose.mat = FALSE)
 	}
+
 	## Enforce scaling and orthogonality constraints
 	## @@@@ FINISH FUNCTION 'make.feasible' so that it handle 
 	## ALL combination of 'ortho' and 'scope' (= 'scale')
@@ -260,15 +236,12 @@ for (l in 1:r) {
 	iters[l] <- out$iters
 	
 	## Post-process canonical weights
-	for (i in 1:length(x)) {
-		singleton <- (dimv[[i]] == 1)
-		if (!any(singleton)) next			
-		vil <- replicate(length(dimv[[i]]), 1, FALSE)
-		if (all(singleton)) {
-			vil[[1]] <- out$v[[i]][[1]]
-		} else {
-			vil[!singleton] <- out$v[[i]]			
-		}
+	for (i in seq_along(xl)) {
+		dimxli <- dimxl[[i]]
+		if (is.null(dimxli) || all(dimxli > 1)) next
+		vil <- vector("list", length(dimxli))
+		vil[dimxli == 1] <- list(1)
+		vil[dimxli > 1] <- out$v[[i]]		
 		out$v[[i]] <- vil		
 	}
 	
