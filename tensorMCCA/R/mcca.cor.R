95169598b111c306a796e116d7974b431592d7f1
David Degras <david.degras@umb.edu>
2026-07-24 13:16:28 -0400

    Update mcca.cor.R

--- tensorMCCA/R/mcca.cor.R
+++ tensorMCCA/R/mcca.cor.R
@@ -12,17 +12,21 @@ stopifnot(r > 0)
 
 ## Data dimensions
 m <- length(x) 
-dimx <- lapply(x, dimfun) 
+for (i in 1:m) { # matricize any vector dataset
+	if (is.vector(x[[i]])) {
+		dim(x[[i]]) <- c(1, length(x[[i]]))
+	}
+}
+dimx <- lapply(x, dim) 
 d <- sapply(dimx, length) - 1L 
 p <- mapply(head, dimx, d, SIMPLIFY = FALSE)
-p[d == 0] <- 1
 n <- tail(dimx[[1]], 1) 
 
 ## Objective weights
 w <- if (length(w) == 1) { 
-	matrix(1 / (m^2), m, m) 
+	matrix(w, m, m) 
 } else { 
-	(w + t(w)) / (2 * sum(w))
+	(w + t(w)) / 2
 }
 
 ## Match other arguments
@@ -44,18 +48,13 @@ if (is.character(init)) {
 		svd = mcca.init.svd, random = mcca.init.random)
 }
 
-## Calculate tensor means across last dimension
-xbar <- vector("list", m)
-uncentered <- logical(m)
+## Center data as needed
 for(i in 1:m) {
-    xbar[[i]] <- if (d[i] == 0) {
-    	mean(x[[i]])
-    } else {
-	    as.vector(rowMeans(x[[i]], dims = d[i]))
+    xbar <- as.vector(rowMeans(x[[i]], dims = d[i]))
+	if (any(abs(xbar) > 1e-16)) {
+		x[[i]] <- x[[i]] - xbar
 	}
-	uncentered[i] <- any(abs(xbar[[i]]) > 1e-16)
 }
-uncentered <- which(uncentered)
 
 ## Adjust number of canonical components
 pp <- sapply(p, prod)
@@ -122,10 +121,8 @@ for (l in 1:r) {
 
 	if (verbose) cat("\n\nMCCA: Component", l, "\n")
 
-	## Create copy of original data before centering and deflating 
+	## Create copy of original data before deflating 
 	xl <- x
-	for (i in uncentered) 
-		xl[[i]] <- xl[[i]] - xbar[[i]]
 	
 	## Prepare orthogonality constraints and deflate data
 	if (l > 1) {
@@ -142,59 +139,32 @@ for (l in 1:r) {
 					ortho.cnstr$modes[[i]])	
 		} 
 	}
-
-	## Remove datasets that are zero 
-	xzero <- sapply(xl, function(a) all(a == 0))
-	if (all(xzero)) {
-		v[,l] <- relist(lapply(unlist(p), numeric), p)
-		objective[l] <- 0
-		if (l > 1 && ortho == "weight") next else break
-	}
-	xnzero <- !xzero
-	if (any(xzero)) xl <- xl[xnzero]
-	wl <- w[xnzero, xnzero]
-
-	# ## Drop singleton dimensions
-	# dimxl <- vector("list", sum(xnzero))
-	# for (i in seq_along(xl)) {
-		# dimxl[[i]] <- dim(xl[[i]])
-		# if (is.null(dimxl[[i]]) || all(dimxl[[i]] > 1)) next
-		# dim(xl[[i]]) <- drop(dimxl[[i]])
-	# }
-
-	
-	## Set canonical weights for datasets equal to zero
-	for (i in which(xzero)) 
-		v[[i,l]] <- lapply(p[[i]], numeric)
-		
+			
 	## Specify orthogonality constraints in optimization
 	cnstr <- if (l == 1 || ortho == "weight") {
 		NULL
 	} else if (ortho == "score") { 
-		ortho.cnstr[xnzero, 1:(l-1), drop = FALSE]
+		ortho.cnstr[, 1:(l-1), drop = FALSE]
 	}
 
 	## Initialize canonical weights
 	if (is.character(init)) {
 		init.args$x <- xl
 		if (init %in% c("cca","svd")) 
-			init.args$w <- wl			
+			init.args$w <- w		
 		v0 <- do.call(mcca.init, init.args)
 	} else if (is.list(init)) { 
 		v0 <- if (is.vector(init)) {
-			init[xnzero] 
+			init 
 		} else { 
-			init[xnzero, min(l, ncol(init))]
+			init[, min(l, ncol(init))]
 		}
 		if (l > 1 && ortho == "weight") 
-			v0 <- tnsr.rk1.mat.prod(v0, mat = ortho.cnstr$mat[xnzero], 
-				modes = ortho.cnstr$modes[xnzero], transpose.mat = FALSE)
+			v0 <- tnsr.rk1.mat.prod(v0, mat = ortho.cnstr$mat, 
+				modes = ortho.cnstr$modes, transpose.mat = FALSE)
 	}
 		
-	## Enforce scaling and orthogonality constraints
-	if (l > 1) {
-		v0 <- tnsr.rk1.ortho(v0, cnstr, maxit = 100L, tol = 1e-6)
-	}
+	## Enforce scaling constraints
 	v0 <- scale.v(v0, type = "var", x = xl, check.args = FALSE)
 	
 	## Run TMCCA
@@ -207,8 +177,8 @@ for (l in 1:r) {
 			tol = tol, verbose = verbose)
 	}
 	objective[l] <- out$objective
-	block.score[,xnzero,l] <- out$score
-	global.score[,l] <- rowMeans(block.score[,xnzero,l])	
+	block.score[,,l] <- out$score
+	global.score[,l] <- rowMeans(block.score[,,l])	
 	iters[l] <- out$iters	
 	
 	# ## Post-process canonical weights
@@ -224,24 +194,24 @@ for (l in 1:r) {
 	## Transform back canonical weights to original search space 
 	if (ortho == "weight" && l > 1) {
 		out$v <- tnsr.rk1.mat.prod(v = out$v, 
-			mat = ortho.cnstr$mat[xnzero], 
-			modes = ortho.cnstr$modes[xnzero], 
+			mat = ortho.cnstr$mat, 
+			modes = ortho.cnstr$modes, 
 			transpose.mat = TRUE)
 	}
-	v[xnzero,l] <- out$v
+	v[,l] <- out$v
 
 	## Check orientation of canonical weights (can the objective be
 	## increased by flipping the orientation of some canonical tensors?)
-	test <- reorient(out$score, w[xnzero,xnzero])
-	if (any(test$flip)) {
-		objective[l] <- test$objective
-		idx <- which(xnzero)[test$flip]
-		for (i in idx) {
-			v[[i,l]][[1]] <- (-v[[i,l]][[1]])
-			block.score[,i,l] <- -block.score[,i,l]
-		}
-		global.score[,l] <- rowMeans(block.score[,,l])
-	}
+	# test <- reorient(out$score, w[xnzero,xnzero])
+	# if (any(test$flip)) {
+		# objective[l] <- test$objective
+		# idx <- which(xnzero)[test$flip]
+		# for (i in idx) {
+			# v[[i,l]][[1]] <- (-v[[i,l]][[1]])
+			# block.score[,i,l] <- -block.score[,i,l]
+		# }
+		# global.score[,l] <- rowMeans(block.score[,,l])
+	# }
 	
 } 
 
